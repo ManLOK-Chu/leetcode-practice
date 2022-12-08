eaa9ecce553b8e9e89227196d1f99b2e5b1719d3
edmondzhu <edmondzhu@tencent.com>
2022-12-08 14:57:13 +0800

    链表相交

--- intersection-of-two-linked-lists/main.go
+++ intersection-of-two-linked-lists/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type ListNode struct {
 	Val  int
@@ -15,38 +17,73 @@ type ListNode struct {
  * }
  */
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
-	var tailA, tailB = headA, headB
-	var lengthA, lengthB int
-	var dummyHeadA, dummyHeadB = &ListNode{Next: headA}, &ListNode{Next: headB}
-	for tailA.Next != nil {
+	var cur = headA
+	var lengthA, lengthB = 1, 1
+	for cur.Next != nil {
 		lengthA++
-		tailA = tailA.Next
+		cur = cur.Next
 	}
-	for tailB.Next != nil {
+	cur = headB
+	for cur.Next != nil {
 		lengthB++
-		tailB = tailB.Next
+		cur = cur.Next
 	}
-	var curA, curB = dummyHeadA.Next, dummyHeadB.Next
-
+	var longCur, shortCur = headA, headB
+	var gap = lengthA - lengthB
+	if lengthB > lengthA {
+		longCur, shortCur = headB, headA
+		gap = lengthB - lengthA
+	}
+	for gap > 0 {
+		longCur = longCur.Next
+		gap--
+	}
+	for longCur != nil {
+		if longCur == shortCur {
+			return longCur
+		}
+		longCur = longCur.Next
+		shortCur = shortCur.Next
+	}
+	return nil
+	//var m = make(map[*ListNode]bool)
+	//var cur = headA
+	//for cur != nil {
+	//	m[cur] = true
+	//	cur = cur.Next
+	//}
+	//cur = headB
+	//for cur != nil {
+	//	if m[cur] {
+	//		return cur
+	//	}
+	//	cur = cur.Next
+	//}
+	//return nil
 }
 
 func main() {
-	fmt.Println(getIntersectionNode(initListNode([]int{4, 1, 8, 4, 5}, []int{5, 6, 1, 8, 4, 5})))
-	fmt.Println(getIntersectionNode(initListNode([]int{1, 9, 1, 2, 4}, []int{3, 2, 4})))
-	fmt.Println(getIntersectionNode(initListNode([]int{2, 6, 4}, []int{1, 5})))
+	fmt.Println(getIntersectionNode(initListNode([]int{4, 1, 8, 4, 5}, []int{5, 6, 1, 8, 4, 5}, 2, 3)))
+	fmt.Println(getIntersectionNode(initListNode([]int{1, 9, 1, 2, 4}, []int{3, 2, 4}, 3, 1)))
+	fmt.Println(getIntersectionNode(initListNode([]int{2, 6, 4}, []int{1, 5}, 3, 2)))
 }
 
-func initListNode(arrA []int, arrB []int) (*ListNode, *ListNode) {
+func initListNode(arrA, arrB []int, skipA, skipB int) (*ListNode, *ListNode) {
 	var dummyHeadA, dummyHeadB = &ListNode{}, &ListNode{}
+	var listA = make([]*ListNode, len(arrA))
 	var cur = dummyHeadA
 	for i := 0; i < len(arrA); i++ {
 		cur.Next = &ListNode{Val: arrA[i]}
 		cur = cur.Next
+		listA[i] = cur
 	}
 	cur = dummyHeadB
-	for i := 0; i < len(arrB); i++ {
+	for i := 0; i < skipB; i++ {
 		cur.Next = &ListNode{Val: arrB[i]}
 		cur = cur.Next
+	}
+	if skipB < len(arrB) {
+		cur.Next = listA[skipA]
 	}
 	return dummyHeadA.Next, dummyHeadB.Next
 }
