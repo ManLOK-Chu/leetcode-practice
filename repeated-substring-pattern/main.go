30389383a53badb7a44866062bd2c58623754983
edmondzhu <edmondzhu@tencent.com>
2022-12-16 19:32:54 +0800

    update

--- repeated-substring-pattern/main.go
+++ repeated-substring-pattern/main.go
@@ -26,14 +26,30 @@ import (
 //	}
 //	return false
 //}
+
+//func repeatedSubstringPattern(s string) bool {
+//	var length = len(s)
+//	var t = s[1:] + s[:length-1]
+//	return strings.Contains(t, s)
+//}
+
 func repeatedSubstringPattern(s string) bool {
 	var length = len(s)
-	var t = s[1:] + s[:length-1]
-	var newLength = (length << 1) - 2
-	for i := 0; i+length <= newLength; i++ {
-		if t[i:i+length] == s {
-			return true
+	var j = 0
+	next := make([]int, length)
+	next[0] = j
+	for i := 1; i < length; i++ {
+		for j > 0 && s[i] != s[j] {
+			j = next[j-1]
 		}
+		if s[i] == s[j] {
+			j++
+		}
+		next[i] = j
+	}
+	// next[length-1]  最长相同前后缀的长度
+	if next[length-1] != 0 && length%(length-next[length-1]) == 0 {
+		return true
 	}
 	return false
 }
