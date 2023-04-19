3f0da56d3d5ea983f399d47aaf7b05a801670641
ManLOKChu <zhuwenle@163.com>
2023-04-20 02:25:34 +0800

    不足k个也要翻转

--- reverse-nodes-in-k-group/main.go
+++ reverse-nodes-in-k-group/main.go
@@ -19,7 +19,7 @@ func (node *ListNode) String() string {
 	return fmt.Sprint(res)
 }
 
-//给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。
+//给你链表的头节点 head ，每k个节点一组进行翻转，请你返回修改后的链表。
 //k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是k的整数倍，那么请将最后剩余的节点保持原有顺序。
 //你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
 func reverseKGroup(head *ListNode, k int) *ListNode {
@@ -32,6 +32,9 @@ func reverseKGroup(head *ListNode, k int) *ListNode {
 			if tail == nil {
 				return dummyHead.Next
 			}
+			//if tail != nil && tail.Next == nil { //不足k个也要翻转
+			//	break
+			//}
 		}
 		//暂存子链表的下一个节点
 		tmp := tail.Next
@@ -57,6 +60,7 @@ func reverseList(head, tail *ListNode) (*ListNode, *ListNode) {
 }
 
 func main() {
+	fmt.Println(reverseKGroup(initListNode([]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}), 4))
 	fmt.Println(reverseKGroup(initListNode([]int{1, 2, 3, 4, 5}), 2))
 	fmt.Println(reverseKGroup(initListNode([]int{1, 2, 3, 4, 5}), 3))
 }
